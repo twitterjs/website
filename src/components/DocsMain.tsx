fc8eb6fbab07668de47d3bc440969856a0495d98
Shubham Parihar <shubhamparihar391@gmail.com>
2022-03-19 21:46:10 +0530

    feat: add a back-to-top button

--- src/components/DocsMain.tsx
+++ src/components/DocsMain.tsx
@@ -1,15 +1,19 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
+import { BackToTop } from './BackToTop';
 import { ClassItem } from './ClassItem';
 import { GettingStarted } from './GettingStarted';
 import { TypedefItem } from './TypedefItem';
 
 export function DocsMain() {
 	return (
-		<Routes>
-			<Route path='' element={<Navigate to='getting-started/general' />} />
-			<Route path='getting-started/:topic' element={<GettingStarted />} />
-			<Route path='classes/:className/*' element={<ClassItem />} />
-			<Route path='typedefs/:typedefName/*' element={<TypedefItem />} />
-		</Routes>
+		<>
+			<Routes>
+				<Route path='' element={<Navigate to='getting-started/general' />} />
+				<Route path='getting-started/:topic' element={<GettingStarted />} />
+				<Route path='classes/:className/*' element={<ClassItem />} />
+				<Route path='typedefs/:typedefName/*' element={<TypedefItem />} />
+			</Routes>
+			<BackToTop />
+		</>
 	);
 }
